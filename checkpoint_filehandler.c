a4ca49bcd93a47c3830a5ec17c01bdf830c1adad
pieterb <pieter0benjamin@gmail.com>
2019-08-15 15:42:45 -0700

    added tree writing functions

--- checkpoint_filehandler.c
+++ checkpoint_filehandler.c
@@ -2,15 +2,19 @@
 
 #include "checkpoint_filehandler.h"
 
+void FileHandlerNullFree(void *val) { }
+
 int WriteCheckPointLog(CheckPointLogPtr cpt_log) {
   // TODO
+  // WriteTree
+  // Write offsets
   return WRITE_SUCCESS;
 }
 
 int ReadCheckPointLog(CheckPointLogPtr cpt_log) {
   struct stat;
   if (DEBUG) {
-    printf("\t\tloading tables ...\n");
+    printf("\t\tloading tables . . .\n");
   }
 
   // Allocate space for the hashtables
@@ -23,8 +27,17 @@ int ReadCheckPointLog(CheckPointLogPtr cpt_log) {
       cpt_log->src_filehash_to_cptname  == NULL ||
       cpt_log->cpt_namehash_to_cptfilename == NULL ||
       cpt_log->dir_tree == NULL) {
+    FreeHashTable(cpt_log->src_filehash_to_filename, &FileHandlerNullFree);
+    FreeHashTable(cpt_log->src_filehash_to_cptname, &FileHandlerNullFree);
+    FreeHashTable(cpt_log->cpt_namehash_to_cptfilename, &FileHandlerNullFree);
+    FreeHashTable(cpt_log->dir_tree, &FreeCpTreeNode);
+    if (DEBUG) {
+      printf("error allocating space for hashables\n");
+    }
     return READ_ERROR;
   }
+
+  // Read the stored tables (if they exist)
   // if (stat(STORED_CPTS_FILE, &stat) == 0
 
   return READ_SUCCESS;
@@ -39,9 +52,9 @@ int WriteSrcToCheckpoint(char *src_filename, char *cpt_name) {
   cpt_filename[dir_len + 1] = '\0';
   strcat(cpt_filename, cpt_name);
 
-  if ((cpt_file = fopen(cpt_filename, "ab+")) == NULL) {
+  if ((cpt_file = fopen(cpt_filename, "wb")) == NULL) {
     fprintf(stderr,
-            "\tError creating file %s%s.\n"\
+            "\tError opening file %s%s.\n"\
             "\tAborting program now.\n", WORKING_DIR, cpt_filename);
     return -2;
   }
@@ -68,4 +81,100 @@ int WriteSrcToCheckpoint(char *src_filename, char *cpt_name) {
   }
 
   return 0;
+}
+
+static int WriteTree(CpTreeNodePtr curr_node, ssize_t offset, FILE *f) {
+  if (curr_node == NULL) {
+    return 0;
+  }
+
+  LLIter it;
+  ssize_t child_bytes_written = 0, name_len, num_children,
+          num_attempts, self_content_length;
+  int res;
+  name_len     = strlen(curr_node->cpt_name) + 1;
+  num_children = LLSize(curr_node->children);
+  ssize_t children_offsets[num_children];
+
+  // A file_treenode is written the following way:
+  //
+  // [name_len][num_children]["name\0"][children_offsets][      children      ]
+  //
+  // name_len and num_children are type ssize_t
+  // name is a char array (which omits the null terminating byte)
+  // children_offsets is an array of ssize_t values
+  //
+  // It's impossible to tell anything about the length of children, unless
+  // num_children is 0, in which case there will not be any children_offsets
+  // or children field written.
+  //
+  // Thus, the length of a "prefix" for a file_treenode can be evaluated as
+  self_content_length = (sizeof(ssize_t) * (2 + num_children));
+                      + (sizeof(char)  * name_len);
+
+  if ((res = WriteChildren(curr_node,
+                           it,
+                           children_offsets,
+                           offset + self_content_length,
+                           f)) == FILE_WRITE_ERR) {
+    return FILE_WRITE_ERR;
+  }
+  child_bytes_written = res;
+
+  // Write self:
+  if (fseek(f, offset, SEEK_SET) != 0) {
+    return FILE_WRITE_ERR;
+  }
+
+  // Unfortunately, we have to do two (often three) writes since
+  // name and children_offsets are variable length
+  FileTreeHeader header = {name_len, num_children};
+  // Write the bookkeeping information.
+  if (fwrite(&header, sizeof(FileTreeHeader), 1, f) != 1) {
+    return FILE_WRITE_ERR;
+  }
+  // Write the name field.
+  if (fwrite(curr_node->cpt_name, sizeof(char) * name_len, 1, f) != 1) {
+    return FILE_WRITE_ERR;
+  }
+  if (num_children > 0) {
+    // If we have any children, write their offsets.
+    if (fwrite(children_offsets, sizeof(ssize_t) * num_children, 1, f) != 1) {
+      return FILE_WRITE_ERR;
+    }
+  }
+
+  // The total amount written for this tree is the amount
+  // written for the current node plus the amount written
+  // for the current nodes children.
+  return self_content_length + child_bytes_written;
+}
+
+static int WriteChildren(CpTreeNodePtr curr_node,
+                         LLIter it,
+                         ssize_t *children_offsets,
+                         ssize_t offset,
+                         FILE *f) {
+  ssize_t self_content_length, res,
+          child_bytes_written = 0,
+          num_attempts = 20,
+          num_children = LLSize(curr_node->children);
+  ATTEMPT( (it = LLGetIter(curr_node->children, 0)), NULL, num_attempts)
+
+  CpTreeNodePtr curr_child;
+  for (ssize_t i = 0; i < num_children; i++) {
+    children_offsets[i] = ((num_children - (i + 1)) * 4) + child_bytes_written;
+    LLIterPayload(it, &curr_child);
+    res = WriteTree(curr_child, offset + child_bytes_written, f);
+
+    if (res == FILE_WRITE_ERR) {  // Something went wrong writing the child.
+      LLIterFree(it);
+      return FILE_WRITE_ERR;
+    }
+
+    child_bytes_written += res;
+  }
+  LLIterFree(it);
+
+  return child_bytes_written;
 }
